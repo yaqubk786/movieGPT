4c2377676b13abcfdffc7c89ce1f2f698eced65c
yaqubk786 <yaqubk477@gmail.com>
2024-12-05 12:25:57 +0530

    tmdb api integrated and teaser video playing

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { AiOutlineCaretRight } from "react-icons/ai";
+import {
+  AiOutlineCaretRight,
+} from "react-icons/ai";
+import { IoIosInformationCircleOutline } from "react-icons/io";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12 absolute text-white bg-gradient-to-r from-black w-screen aspect-video">
+    <div className="pt-[20%] px-12 absolute text-white bg-gradient-to-r from-black w-screen aspect-video">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
       <div className="flex">
-        <button className="bg-gray-500 text-white p-3 px-3 w-40 flex justify-center items-center text-xl bg-opacity-50 rounded-lg mr-2 ">
+        <button className="bg-white text-black p-3 px-3 w-40 flex justify-center items-center text-xl bg-opacity-50 rounded-lg mr-2 hover:bg-opacity-80 ">
           <AiOutlineCaretRight className="mr-2" /> Play
         </button>
-        <button className="bg-gray-500 text-white p-3 px-3 w-40  text-xl bg-opacity-50 rounded-lg mr-2">
+        <button className="bg-gray-500 text-white p-3 px-3 w-40 flex justify-center items-center  text-xl bg-opacity-50 rounded-lg mr-2 hover:bg-opacity-80 ">
+          <IoIosInformationCircleOutline className="mr-2"/>
           More Info
         </button>
       </div>
